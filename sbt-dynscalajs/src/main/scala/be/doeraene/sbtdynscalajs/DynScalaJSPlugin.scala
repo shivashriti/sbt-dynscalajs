f3605cf69292d48ba34bbe0b0faa03b54883a20d
Sébastien Doeraene <sjrdoeraene@gmail.com>
2017-11-29 21:26:46 +0100

    WiP Upgrade to Scala.js 1.0.0-M2.
    This doesn't work yet.

--- sbt-dynscalajs/src/main/scala/be/doeraene/sbtdynscalajs/DynScalaJSPlugin.scala
+++ sbt-dynscalajs/src/main/scala/be/doeraene/sbtdynscalajs/DynScalaJSPlugin.scala
@@ -2,6 +2,10 @@ package be.doeraene.sbtdynscalajs
 
 import scala.language.reflectiveCalls
 
+import scala.annotation.tailrec
+
+import java.util.concurrent.atomic.AtomicReference
+
 import sbt._
 import sbt.Keys._
 
@@ -14,10 +18,30 @@ import org.scalajs.core.tools.linker.ModuleKind
 import org.scalajs.jsenv._
 import org.scalajs.jsenv.nodejs.NodeJSEnv
 
-import org.scalajs.testadapter.FrameworkDetector
+import org.scalajs.testadapter.TestAdapter
 
 object DynScalaJSPlugin extends AutoPlugin {
   override def requires: Plugins = plugins.JvmPlugin
+
+  @tailrec
+  final private def registerResource[T <: AnyRef](
+      l: AtomicReference[List[T]], r: T): r.type = {
+    val prev = l.get()
+    if (l.compareAndSet(prev, r :: prev)) r
+    else registerResource(l, r)
+  }
+
+  private val createdTestAdapters =
+    new AtomicReference[List[TestAdapter]](Nil)
+
+  private def newTestAdapter(jsEnv: ComJSEnv, jsFiles: Seq[VirtualJSFile],
+      config: TestAdapter.Config): TestAdapter = {
+    registerResource(createdTestAdapters,
+        new TestAdapter(jsEnv, jsFiles, config))
+  }
+
+  private def closeAllTestAdapters(): Unit =
+    createdTestAdapters.getAndSet(Nil).foreach(_.close())
 
   object autoImport {
     // Stage values
@@ -211,6 +235,16 @@ object DynScalaJSPlugin extends AutoPlugin {
       },
 
       scalaJSStage := FastOptStage,
+
+      // Close all test adapters every time a sequence of tasks ends
+      onComplete := {
+        val prev = onComplete.value
+
+        { () =>
+          prev()
+          closeAllTestAdapters()
+        }
+      },
   )
 
   private def stageSettings(stage: Stage,
@@ -418,6 +452,8 @@ object DynScalaJSPlugin extends AutoPlugin {
 
               val s = streams.value
 
+              val frameworks = testFrameworks.value
+
               val env = jsEnv.value match {
                 case env: ComJSEnv =>
                   env
@@ -432,13 +468,19 @@ object DynScalaJSPlugin extends AutoPlugin {
               val moduleKind = ModuleKind.NoModule
               val moduleIdentifier = None
 
-              val frameworksAndTheirImplNames =
-                testFrameworks.value.map(f => f -> f.implClassNames.toList)
+              val frameworkNames = frameworks.map(_.implClassNames.toList).toList
 
-              val logger = Loggers.sbtLogger2ToolsLogger(s.log)
+              val logger = Loggers.sbtLogger2ToolsLogger(streams.value.log)
+              val config = TestAdapter.Config()
+                .withLogger(logger)
+                .withModuleSettings(moduleKind, moduleIdentifier)
 
-              FrameworkDetector.detectFrameworks(env, files, moduleKind,
-                  moduleIdentifier, frameworksAndTheirImplNames, logger)
+              val adapter = newTestAdapter(env, files, config)
+              val frameworkAdapters = adapter.loadFrameworks(frameworkNames)
+
+              frameworks.zip(frameworkAdapters).collect {
+                case (tf, Some(adapter)) => (tf, adapter)
+              }.toMap
             }
         }
       }.value,
